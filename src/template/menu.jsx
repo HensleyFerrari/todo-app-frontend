5d860d9a3963893eaad195ed8ec2a74471b221ea
Hensley Ferrari <hensley_ferrari@hotmail.com>
2020-08-02 18:00:44 -0300

    Component menu

---
+++ src/template/menu.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+export default props => (
+    <nav className='navbar navbar-expand-lg navbar-dark bg-dark'>
+        <div className='container'>     
+            <a className='navbar-brand' href="#">TodoApp</a>
+
+            <div className="navbar-collapse collapse" id="navbarNav">
+                <ul className="navbar-nav">
+                    <li className='nav-item'>
+                        <a className="nav-link" href="#/todos">Tasks</a>
+                    </li>
+                    <li className='nav-item'>
+                        <a className="nav-link" href="#/about">About</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+)
