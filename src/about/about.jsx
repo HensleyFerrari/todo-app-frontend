f2546886b42b684d973fce2a86ef095ac4be20e5
Hensley Ferrari <hensley_ferrari@hotmail.com>
2020-08-03 23:12:52 -0300

    Component PageHeader

--- src/about/about.jsx
+++ src/about/about.jsx
@@ -1,7 +1,22 @@
 import React from 'react'
 
+import PageHeader from '../template/pageHeader'
+
 export default props => (
     <div>
-        <h1>Sobre</h1>
+        <PageHeader name='About' small='The company'/>
+
+        <h2 className='mt-4'>Our history</h2>
+        <p>Lorem ipsum dolor, sit amet consectetur
+             adipisicing elit. Mollitia, suscipit eveniet 
+             accusantium iusto velit fuga praesentium quod adipisci 
+             sunt officiis numquam sed cumque laboriosam 
+            quia incidunt animi, magnam similique impedit.</p>
+        <h2>Our Mission</h2>
+        <p>Lorem ipsum dolor, sit amet consectetur
+             adipisicing elit. Mollitia, suscipit eveniet 
+             accusantium iusto velit fuga praesentium quod adipisci 
+             sunt officiis numquam sed cumque laboriosam 
+            quia incidunt animi, magnam similique impedit.</p>
     </div>
 )
