0be452e327854f486e898f2145264dbd70436497
Hensley Ferrari <hensley_ferrari@hotmail.com>
2020-08-04 10:51:55 -0300

    Read and Delete task

--- src/todo/todoList.jsx
+++ src/todo/todoList.jsx
@@ -1,7 +1,31 @@
 import React from 'react'
+import { render } from 'react-dom'
+import IconButton from '../template/iconButton'
 
-export default props => (
-    <div>
-        <h1>List</h1>
-    </div>
-)
+export default props => {
+
+    const renderRows = () => {
+        const list = props.list || []
+        return list.map(todo => (
+            <tr key={todo._id}>
+                <td>{todo.description}</td>
+                <IconButton style='danger' icon='trash-o' 
+                    onClick={() => props.handleRemove(todo)}/>
+            </tr>
+        ))
+    }
+
+    return (
+        <table className='table mt-2'>
+            <thead>
+                <tr>
+                    <th>Description</th>
+                    <th>Actions</th>
+                </tr>
+            </thead>
+            <tbody>
+                {renderRows()}
+            </tbody>
+        </table>
+    )
+}
