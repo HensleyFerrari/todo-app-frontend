23e0aa0d2a344bc686216a511533c22a94ea92ac
Hensley Ferrari <hensley_ferrari@hotmail.com>
2020-08-04 10:35:55 -0300

    Add task event (backend)

--- src/todo/todoForm.jsx
+++ src/todo/todoForm.jsx
@@ -9,10 +9,14 @@ export default props => (
     <div role='form' className='todoForm mt-2 form-row'>
         <Grid cols='12 9 10'>
             <input id='description' className='form-control'
-                placeholder='Add task' />
+                placeholder='Add task'
+                value={props.description} 
+                onChange={props.handleChange}
+                />
         </Grid>
         <Grid cols='12 3 2'>
-            <IconButton style='primary' icon='plus'></IconButton>
+            <IconButton style='primary' icon='plus' 
+                onClick={props.handleAdd}></IconButton>
         </Grid>
     </div>
 )
